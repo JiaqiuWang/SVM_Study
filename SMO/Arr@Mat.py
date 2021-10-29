38a324a4fba4ecdae446ed9b80819645988c1e1a
JiaqiuWang <wangjiaqiu186@gmail.com>
2021-10-29 17:08:43 +0800

    数组与矩阵学习

---
+++ SMO/Arr@Mat.py
@@ -0,0 +1,80 @@
+"""
+描述：Python中向量、矩阵学习
+作者：王佳秋
+日期：2021年10月29日
+"""
+
+import numpy as np
+
+
+"""一.一维向量（行、列）：
+在numpy中一维向量用一位数组array([1, 1, 1])表示，既能表示行向量也能表示列向量，一维向量转置后还是原来的样子（因为存储结构是数组）
+"""
+# 1.向量、矩阵表示：numpy的向量表示是通过array数组来实现的
+v1 = np.array([1, 2, 8])
+v1T1 = v1.transpose()  # 转置矩阵
+v1T2 = v1.T  # 转置矩阵
+print("向量\nv1:", v1, ", type:", type(v1))
+print("v1.transpose():", v1T1)
+print("v1.T:", v1T2)
+
+# 2.矩阵
+v1 = np.array([[1, 1, 1]])  #
+print("矩阵\nv1:", v1, ", type:", type(v1))
+# 两行三列的矩阵
+v2 = np.array([[1, 1, 2], [1, 1, 0]])
+print("矩阵\nv2:", v2, ", type:", type(v2))
+
+
+"""二.向量计算：线性代数矩阵乘法"""
+# 1. 线性代数矩阵乘法，行乘列，再相加
+v1 = np.array([1, 0, 1])
+v11 = np.array([1, 1, 1])
+v2 = np.array([[1, 1, 2], [1, 1, 0]])
+print("一维向量dot:", np.dot(v1, v11))
+print("一维向量*二维向量：", np.dot(v2, v1))
+
+
+"""三.对应位置相乘"""
+print("一维向量multiply:", np.multiply(v1, v1))
+print("一维向量*:", v1 * v11)
+print("一维向量multiply二维向量：\n", np.multiply(v1, v2), ", type:", type(np.multiply(v1, v2)))
+
+
+"""四.加减法"""
+print("一维向量相加：", v1 + v11)
+print("二维向量相加：", v1 + v2)
+print("一维向量向量相减：", v1 - v11)
+print("一维向量减二维向量：", v1 - v2)
+
+
+"""五.矩阵的另一种表示mat
+(1) 在创建矩阵的专用字符串中，矩阵的行与行之间用分号隔开，行内的元素之间用空格隔开。
+(2) 用T属性获取转置矩阵
+"""
+# 1.创建矩阵：mat函数创建任意矩阵
+a = np.mat('1 2 3; 4 5 6')
+print("mat函数创建的任意矩阵：\n", a, ", type:", type(a))
+aTp = a.transpose()
+print("a的转置矩阵transpose：\n", aTp)
+aT = a.T
+print("a的转置矩阵transpose：\n", aT)
+B = np.mat(np.arange(12).reshape(3, 4))
+print("元素值为0-11的3*4维矩阵：\n", B)
+
+
+"""六.构建全0矩阵"""
+A = np.zeros((4, 5))
+print("全0矩阵：\n", A)
+
+
+"""七.构建单位矩阵"""
+C = np.eye(3)
+print("单位矩阵：\n", C)
+
+
+"""八.构建符合矩阵"""
+D = C * 2
+print("单位矩阵*2：\n", D)
+E = np.bmat('C D; D C')
+print("连接bind符合矩阵：\n", E)
--- SMO/SMO.py
+++ SMO/SMO.py
@@ -21,12 +21,12 @@ class SMOStruct:
         self.kernel = kernel  # kernel function 核函数，实现了两个核函数：线性与高斯(RBF)核函数
         self.alphas = alpha  # lagrange multiplier拉格朗日乘子，与样本一一相对
         self.b = b  # scalar bias term 标量，偏移量
-        self.errors = errors  # error cache 差值矩阵，用于存储判别函数实际值与预测值的差值，与样本一一相对
+        self.errors = errors  # error cache 输出值误差：差值矩阵，用于存储判别函数实际值与预测值的差值，与样本一一相对
         # 存储的目的为递归、优化使得样本能够快速迭代，找到alpha值
         self.m, self.n = np.shape(self.X)  # store size(m) of training set and the
-        # number of features(n) for each example
-        self.user_linear_optim = user_linear_optim  # 判断模型是否使用线性核函数
-        self.w = np.zeros(self.n)  # 初始化权重w的值，主要用于线性核函数
+        # number of features(n) for each example训练 样本的个数和每个样本的features数量
+        self.user_linear_optim = user_linear_optim  # Boolean值，判断模型是否使用线性核函数
+        self.w = np.zeros(self.n)  # 初始化权重w的值，用于线性核函数
 
 
 def linear_kernel(x, y, b=1):
@@ -34,7 +34,31 @@ def linear_kernel(x, y, b=1):
     # returns the linear combination of arrays 'x' and 'y' with the optional
     # bias term 'b' (set to 1 by default).
     result = x @ y.T + b
+    print("x ")
     return result  # Note the @ operator for matrix multiplications
+
+
+def decision_function_output(model, i):
+    """判别函数1：用于单一样本"""
+    if model.user_linear_optim:
+        # 线性决策函数 Equation (J1)
+        # return float(np.dot(model.w.T, model.X[i]) - model.b)
+        return float(model.w.T @ model.X[i]) - model.b
+    else:
+        # 非线性决策函数 Equation (J10)
+        return np.sum([model.alphas[j] * model.y[j] * model.kernel(model.X[j], model.X[i])
+                      for j in range(model.m)]) - model.b
+
+
+def decision_function(alphas, target, kernel, X_train, x_test, b):
+    """判别函数2：用于多个样本
+    Applies the SVM decision function to the input feature vectors in 'x_test'. """
+    result = (alphas * target)
+    result = kernel(X_train, x_test)
+    print("result:", result)
+    # result = (alphas * target) @ kernel(X_train, x_test) - b  # * . @ 两个Operators的区别
+
+    return result
 
 
 def main():
@@ -49,19 +73,27 @@ def main():
     y[y == 0] = -1
 
     # set model parameter and initial values
-    C = 20.0  # 正则化超参，目标函数的约束   s.t. 0<alpha<C
+    C = 20.0  # 正则化超参，目标函数的约束   s.t. 0<=alpha<=C
     m = len(X_train_scaled)  # 训练样本的数量
     initial_alphas = np.zeros(m)  # 模型参数，每个样本对应一个alpha值，大多数样本的alpha值为0
-    # 只有在support hyperplane之间的为C，之外的为0.
+    # 只有在support hyperplane之间的为C，之外的为0，在线之上为0<=alpha<=C
     initial_b = 0.0  # 截距
 
     # set tolerances  容差
-    tol = 0.01  # error tolerance 差值EI的容差
-    eps = 0.01  # alpha tolerance
+    tol = 0.01  # error tolerance 差值EI的容差。输出误差值=f(xi)-yi的值
+    eps = 0.01  # alpha tolerance 参数alpha误差值=alpha_new - alpha_old
 
     # Instantiate model
-    model = SMOStruct(X_train_scaled, y, C, linear_kernel, initial_alphas, initial_b,
-                      np.zeros(m), user_linear_optim=True)
+    model = SMOStruct(X=X_train_scaled, y=y, C=C, kernel=linear_kernel,
+                      alpha=initial_alphas, b=initial_b, errors=np.zeros(m),
+                      user_linear_optim=True)
+    print("Model created...")
+    # print("errors:", model.errors, ", shape:", model.errors.shape)
+    # Instantiate 差值矩阵
+    initial_error = decision_function(alphas=model.alphas, target=model.y, kernel=model.kernel,
+                                      X_train=model.X, x_test=model.X, b=model.b) - model.b
+
+    # model.errors = initial_error
 
     print("End!")
 
